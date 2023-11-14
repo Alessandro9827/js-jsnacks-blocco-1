58d110fb0cf87a141d37230a43354451415dc3b3
Alessandro9827 <ale.moramarco27@gmail.com>
2023-11-14 12:34:50 +0100

    snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,12 +1,21 @@
-const firstUserAge = parseInt(prompt('Quanti anni ha il primo utente?'))
-const secondUserAge = parseInt(prompt('Quanti anni ha il secondo utene?'))
+// Input dell'utente
+let kmTrip = parseInt(prompt("Inserisci i Km che vuoi percorrere: "));
+let userAge = parseInt(prompt("Inserisci la tua eta': "));
 
-if (firstUserAge > secondUserAge) {
-    console.log('Il primo utente è il più grande')
+// Calcolo del prezzo senza sconto
+let ticketPrice = kmTrip * 0.21;
+
+// Dichiarazione variabile dello sconto
+let sale;
+
+// Calcolo dello sconto se disponibile
+if (userAge < 18) {
+    sale = ticketPrice * 20 / 100;
+    ticketPrice -= sale;
+} else if (userAge > 65) {
+    sale = ticketPrice * 40 / 100;
+    ticketPrice -= sale;
 }
-else if (secondUserAge > firstUserAge) {
-    console.log('Il secondo utenete è il più grande')
-}
-else {
-    console.log ('Entrambi hanno la stessa età')
-}
+
+// Stampa del prezzo nel documento
+document.getElementById("output").innerHTML = "Il Biglietto del treno costa: " + "<strong>" + ticketPrice.toFixed(2) + "</strong>";
